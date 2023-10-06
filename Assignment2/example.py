b72da4725d9e00d28d80622cffc563e482c12937
Daniel Edvardsen <danedvar@gmail.com>
2023-10-06 19:56:11 +0200

    Added a bunch of queries, missing 8 and 10

--- Assignment2/example.py
+++ Assignment2/example.py
@@ -14,6 +14,10 @@ class ExampleProgram:
         # self.db_connection.query('SET GLOBAL interactive_timeout=28800')
         # self.db_connection.query('SET GLOBAL wait_timeout=28800')
         self.cursor = self.connection.cursor
+        self.counter_ignored = 0
+        self.counter_trackpoints = 0
+        self.counter_transportation = 0
+        self.counter_transportation_ignored = 0
 
     def create_table(self, query, table_name):
         try:
@@ -72,6 +76,7 @@ class ExampleProgram:
     def insert_transportation_into_activity(self, labeled_activities):
         # TODO, this can be done much quicker if fixed before inserting activities into database
         filtered_activities = []
+        length = len(labeled_activities)
         for activity in labeled_activities:
             user_id, transportation_mode, start_date_time, end_date_time = activity
             # Check that the user has labels.txt
@@ -79,22 +84,29 @@ class ExampleProgram:
             # self.cursor.execute("SELECT has_labels FROM User WHERE id = %s", (user_id,))
             # has_labels = self.cursor.fetchone()[0]
             # if has_labels == 1:
+            # First we obtain all the start and end dates from each activity for the user
             self.cursor.execute("SELECT start_date_time, end_date_time FROM Activity WHERE user_id = %s", (user_id,))
             dates = self.cursor.fetchall()
             for dates_db in dates:
+                # We check if any of these start - end date pairs match up exactly with the labeled activity
                 start_date_db, end_date_db = dates_db
-                if start_date_time == start_date_db and end_date_time <= end_date_db:
+                if start_date_time == start_date_db and end_date_time == end_date_db:
                     # print("found match for user: " + user_id + " " + str(start_date_time) + " " + str(end_date_time))
                     values = (transportation_mode, user_id, start_date_time)
                     filtered_activities.append(values)
+                    self.counter_transportation += 1
                     break
                 else:
+                    self.counter_transportation_ignored += 1
                     continue
             # else:
                 # print("User " + user_id + " has no labels.txt, but isn't listed as labeled in labeled_ids.txt")
         query = ("UPDATE Activity SET transportation_mode = %s WHERE user_id = %s AND start_date_time = %s")
-        self.cursor.executemany(query, filtered_activities)
-        self.db_connection.commit()
+        print("ignored " + str(self.counter_transportation_ignored) + " labeled activities")
+        print("inserted " + str(self.counter_transportation) + " labeled activities")
+        print(length - self.counter_transportation, "labeled activities with no date match")
+        # self.cursor.executemany(query, filtered_activities)
+        # self.db_connection.commit()
         
     def insert_trackpoints(self, trackpoints):
         query = (
@@ -113,6 +125,206 @@ class ExampleProgram:
             self.cursor.executemany(query, tuple)
             self.db_connection.commit()
 
+    def query1(self):
+        # how many users, activities and trackpoints are there in the dataset
+        query = (
+            """
+            SELECT 
+            (SELECT COUNT(*) FROM User) AS users, 
+            (SELECT COUNT(*) FROM Activity) AS activities, 
+            (SELECT COUNT(*) FROM TrackPoint) AS trackpoints
+            """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+    
+    def query2(self):
+        query = (
+            """
+            SELECT avg(number_of_activities), min(number_of_activities), max(number_of_activities) 
+                FROM 
+                (
+                    SELECT count(Activity.user_id) as number_of_activities
+                    FROM User
+                    LEFT JOIN Activity
+                    ON (Activity.user_id = query1.id)
+                    GROUP BY query1.id
+                ) AS t;
+            """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        
+    def query3(self):
+        query = (
+            """
+            SELECT user_id, COUNT(*) AS activity_count 
+            FROM Activity 
+            GROUP BY user_id 
+            ORDER BY activity_count 
+            DESC LIMIT 15
+            """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        
+    def query4(self):
+        query = (
+            """
+            SELECT 
+                DISTINCT user_id AS user_id
+            FROM 
+                Activity
+            WHERE 
+                transportation_mode = 'bus';
+        """
+                    )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        print(len(result))
+        
+    def query5(self):
+        query = (
+            """
+            SELECT user_id, COUNT(DISTINCT transportation_mode) AS transportation_modes_count 
+            FROM Activity 
+            GROUP BY user_id 
+            ORDER BY transportation_modes_count 
+            DESC LIMIT 10
+            """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        
+    def query6(self):
+        query = (
+            """
+            SELECT 
+            user_id,
+            start_date_time,
+            end_date_time,
+            COUNT(*) AS occurrence_count
+            FROM 
+                Activity
+            GROUP BY 
+                user_id, start_date_time, end_date_time
+            HAVING 
+                COUNT(*) > 1;
+        """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        
+    def query7a(self):
+        query = (
+            """
+            SELECT COUNT(DISTINCT user_id) AS overnight_users 
+            FROM Activity 
+            WHERE DATE(start_date_time) = DATE(end_date_time - INTERVAL 1 DAY)
+            """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        
+    def query7b(self):
+        query = (
+            """SELECT user_id, 
+            transportation_mode, 
+            TIMESTAMPDIFF(MINUTE, start_date_time, end_date_time) AS duration_minutes 
+            FROM Activity 
+            WHERE DATE(start_date_time) = DATE(end_date_time - INTERVAL 1 DAY) 
+            ORDER BY user_id
+            """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        print(len(result))
+        
+    def query8(self):
+        query = (
+            "SELECT (SELECT COUNT(*) FROM User) AS users, (SELECT COUNT(*) FROM Activity) AS activities, (SELECT COUNT(*) FROM TrackPoint) AS trackpoints"
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        
+    def query9(self):
+        query = (
+            "SELECT (SELECT COUNT(*) FROM User) AS users, (SELECT COUNT(*) FROM Activity) AS activities, (SELECT COUNT(*) FROM TrackPoint) AS trackpoints"
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        
+    def query10(self):
+        query = (
+            "SELECT (SELECT COUNT(*) FROM User) AS users, (SELECT COUNT(*) FROM Activity) AS activities, (SELECT COUNT(*) FROM TrackPoint) AS trackpoints"
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        
+    def query11(self):
+        query = (
+        """
+        SELECT u.id AS user_id, COUNT(*) AS invalid_activity_count 
+        FROM User AS u JOIN Activity AS a ON u.id = a.user_id LEFT JOIN (SELECT t1.activity_id, COUNT(*) AS invalid_tp_count 
+        FROM TrackPoint AS t1 LEFT JOIN TrackPoint AS t2 ON t1.activity_id = t2.activity_id AND t1.id = t2.id + 1 
+        WHERE TIMESTAMPDIFF(MINUTE, t2.date_time, t1.date_time) >= 5 GROUP BY t1.activity_id) AS invalid_t ON a.id = invalid_t.activity_id 
+        WHERE invalid_t.invalid_tp_count IS NOT NULL GROUP BY u.id;
+        """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        print(len(result))
+        
+    def query12(self):
+        query = (
+        """
+        SELECT user.id AS user_id,
+               user.has_labels,
+               COALESCE(most_used_mode, 'No Activities') AS most_used_transportation_mode
+        FROM User user
+        LEFT JOIN (
+            SELECT query1.user_id, 
+                   MAX(transp_count) AS max_count
+            FROM (
+                SELECT user_id, 
+                       transportation_mode,
+                       COUNT(*) AS transp_count
+                FROM Activity
+                WHERE transportation_mode IS NOT NULL
+                GROUP BY user_id, transportation_mode
+            ) AS query1
+            GROUP BY query1.user_id
+        ) AS max_counts
+        ON user.id = max_counts.user_id
+        LEFT JOIN (
+            SELECT user_id, 
+                   transportation_mode AS most_used_mode,
+                   COUNT(*) AS transp_count
+            FROM Activity
+            WHERE transportation_mode IS NOT NULL
+            GROUP BY user_id, transportation_mode
+        ) AS query2
+        ON user.id = query2.user_id AND query2.transp_count = max_counts.max_count
+        WHERE user.has_labels = true;
+        """
+        )
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=self.cursor.column_names))
+        print(len(result))
+        
     def insert_data_into_sql(self):
 
         print("Starting...")
@@ -165,6 +377,7 @@ class ExampleProgram:
                             for i, line in enumerate(file):
                                 # ignore first 6 lines, rest is added to trackpoints array
                                 if i > 6:
+                                    self.counter_trackpoints += 1
                                     columns = line.strip().split(',')
                                     lat, lon, alt, date_days, date, the_time = columns[0], columns[1], columns[3], columns[4], columns[5], columns[6]
                                     date_time = date + " " + the_time
@@ -187,21 +400,25 @@ class ExampleProgram:
                                     end_date_time = datetime.strptime(end_date_time_str, '%Y-%m-%d %H:%M:%S')
                                     activities.append((activities_id ,user_id, None, start_date_time, end_date_time))
                                     activities_id += 1
+                        else:
+                            self.counter_ignored += 1
                   
         tok = time.time()
         print("got users, activities and trackpoints after: " + str(tok - tik) + " seconds.")
+        print("ignored " + str(self.counter_ignored) + " files")
+        print("obtained " + str(self.counter_trackpoints) + " trackpoints")
         # Sort the user data and then insert it into the database
         sorted_users = sorted(users, key=lambda x: x[0])
         print("inserting users into db...")
         tik = time.time()
-        self.insert_into_user(sorted_users)
+        # self.insert_into_user(sorted_users)
         tok = time.time()
         print("finished inserting users after: " + str(tok - tik) + " seconds.")
         
         # Insert the activity data into the database
         print("inserting activities into db...")
         tik = time.time()
-        self.insert_into_activity(activities)
+        # self.insert_into_activity(activities)
         tok = time.time()
         print("finished inserting activities after: " + str(tok - tik) + " seconds.")
         labeled_activities = []
@@ -227,7 +444,7 @@ class ExampleProgram:
         
         print("inserting trackpoints into db...")
         tik = time.time()
-        self.insert_trackpoints(trackpoints)
+        # self.insert_trackpoints(trackpoints)
         tok = time.time()
         print("finished inserting trackpoints after: " + str(tok - tik) + " seconds.")
         
@@ -293,16 +510,30 @@ def main():
         # _ = program.fetch_data(table_name="Person")
 
         # Display the tables
-        program.show_tables()
+        # program.show_tables()
 
-        program.insert_data_into_sql()
+        # Run the program:
+        # program.insert_data_into_sql()
+        
+        # Run the queries:
+        # program.query1()
+        # program.query2()
+        # program.query3()
+        # program.query4()
+        # program.query5()
+        # program.query6()
+        # program.query7a()
+        # program.query7b()
+        # program.query8()
+        # program.query9()
+        # program.query10()
+        # program.query11()
+        # program.query12()
         
         # Delete the tables we created
         # Important that we do it in this order so that we don't break any foreign key constraints
-        
-
         # Jada
-        program.show_tables()
+        # program.show_tables()
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
